b1baa1785c59739df68b0847aff85096c8e44934
Andrii <andriipigal@gmail.com>
2024-08-09 14:53:38 +0200

    fix: fix bug connected with rests in brackets queue

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 module.exports = function check(str, bracketsConfig) {
     let tmp = str.split("");
+
     if (str.length % 2 === 1) {
         console.log(false);
         return false;
@@ -18,74 +19,18 @@ module.exports = function check(str, bracketsConfig) {
     while (tmp.length > 0) {
         let char = tmp.shift();
 
-        if (objOpens[char] !== undefined && objCLoses[char] !== undefined) {
+        if (objOpens[char] === objCLoses[char]) {
             if (q.length > 0 && q[q.length - 1] === char)
                 q.pop();
             else
                 q.push(char);
         } else if (objOpens[char] === undefined) {
-            if (q.length === 0 || q.pop() !== objCLoses[char]) {
-                console.log(false);
+            if (q.length === 0 || q.pop() !== objCLoses[char])
                 return false;
-            }
+
         } else
             q.push(char);
     }
 
-    console.log(true);
-    return true;
+    return (q.length === 0);
 }
-function check(str, bracketsConfig) {
-    let tmp = str.split("");
-    if (str.length % 2 === 1) {
-        console.log(false);
-        return false;
-    }
-
-    let q = [];
-    let objOpens = {};
-    let objCLoses = {};
-
-    for (let i = 0, len = bracketsConfig.length; i < len; i++) {
-        objOpens[bracketsConfig[i][0]] = bracketsConfig[i][1];
-        objCLoses[bracketsConfig[i][1]] = bracketsConfig[i][0];
-    }
-
-
-    while (tmp.length > 0) {
-        let char = tmp.shift();
-
-        if (objOpens[char] !== undefined && objCLoses[char] !== undefined) {
-            if (q.length > 0 && q[q.length - 1] === char)
-                q.pop();
-            else
-                q.push(char);
-        } else if (objOpens[char] === undefined) {
-            if (q.length === 0 || q.pop() !== objCLoses[char]) {
-                console.log(false);
-                return false;
-            }
-        } else
-            q.push(char);
-    }
-
-    console.log(true);
-    return true;
-}
-
-check('()', [['(', ')']]) // -> true
-check('((()))()', [['(', ')']]) // -> true
-check('())(', [['(', ')']]) // -> false
-check('([{}])', [['(', ')'], ['[', ']'], ['{', '}']]) // -> true
-check('[(])', [['(', ')'], ['[', ']']]) // -> false
-check('[]()', [['(', ')'], ['[', ']']]) // -> true
-check('[]()(', [['(', ')'], ['[', ']']]) // -> false
-
-// special case: opening and closing bracket can be the same :)
-
-console.log();
-
-check('||', [['|', '|']]) // -> true
-check('|()|', [['(', ')'], ['|', '|']]) // -> true
-check('|(|)', [['(', ')'], ['|', '|']]) // -> false
-check('|()|(||)||', [['(', ')'], ['|', '|']]) // -> true);
